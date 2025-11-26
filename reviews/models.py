dcc90c5a82c6e13f559d9277127713bfca2bc5ab
ShutenkoStas <shotenko494@gmail.com>
2025-11-26 21:30:57 +0200

    add two models Product, Review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField()
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = "name"
+
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.IntegerField()
+    def __str__(self):
+        return f"{self.product} - {self.rating}"
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+        ordering = "rating"
+
+
+
