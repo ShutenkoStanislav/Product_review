34f4f14a4e4150fa88966e1d803f7f8b9168e659
ShutenkoStas <shotenko494@gmail.com>
2025-11-26 23:58:11 +0200

    add /templates to Base_dir in setting add url for home and product_list add all urls , add 2 views home and product_list, make product_list templates and homa_page little bit add in all templates add header

--- product_review/settings.py
+++ product_review/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'product_review.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from reviews.models import Review, Product
+
+def home(request):
+    return render(request, template_name="review/home.html")
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        "products" : products
+    }
+
+    return render(request, template_name="review/product_list.html", context=context)
 
 
 #  • Форма повинна дозволяти ввести автора,
